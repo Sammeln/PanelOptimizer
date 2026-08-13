3e2509c83a5276d053400c35b97ad7ce84a7c946
Sammeln <40733234+Sammeln@users.noreply.github.com>
2026-08-13 09:54:33 +0300

    Add panel offcut model

---
+++ AGR_PanelOptimizer.Core/Models/PanelOffcut.cs
@@ -0,0 +1,10 @@
+﻿namespace AGR_PanelOptimizer.Core.Models;
+
+public class PanelOffcut
+{
+    public int Length { get; init; }
+
+    public int Height { get; init; }
+
+    public int SourcePanelPosition { get; init; }
+}
