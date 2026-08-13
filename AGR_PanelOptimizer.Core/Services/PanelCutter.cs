20c31ba4c10c1aa1733bbd0d174f33790ba0453c
Sammeln <40733234+Sammeln@users.noreply.github.com>
2026-08-13 09:54:51 +0300

    Add panel cutter

---
+++ AGR_PanelOptimizer.Core/Services/PanelCutter.cs
@@ -0,0 +1,48 @@
+﻿using AGR_PanelOptimizer.Core.Enums;
+using AGR_PanelOptimizer.Core.Models;
+
+namespace AGR_PanelOptimizer.Core.Services;
+
+public class PanelCutter
+{
+    public PanelCutResult Cut(Panel panel, int blankHeight)
+    {
+        if (blankHeight <= 0)
+            throw new ArgumentOutOfRangeException(nameof(blankHeight));
+
+        if (blankHeight > panel.Length)
+            throw new ArgumentException("Высота заготовки не может быть больше длины панели.", nameof(blankHeight));
+
+        var blankCount = panel.Length / blankHeight;
+        var remainingLength = panel.Length % blankHeight;
+
+        var blanks = Enumerable.Range(0, blankCount)
+            .Select(position => new Blank
+            {
+                Length = panel.Height,
+                Height = blankHeight,
+                LeftEdge = EdgeType.Tongue,
+                RightEdge = EdgeType.Groove,
+                SourcePanelPosition = position
+            })
+            .ToArray();
+
+        var offcuts = remainingLength == 0
+            ? Array.Empty<PanelOffcut>()
+            : new[]
+            {
+                new PanelOffcut
+                {
+                    Length = panel.Height,
+                    Height = remainingLength,
+                    SourcePanelPosition = blankCount
+                }
+            };
+
+        return new PanelCutResult
+        {
+            Blanks = blanks,
+            Offcuts = offcuts
+        };
+    }
+}
