b6b00933ebd53ce40e18b97b0690903ca8768e4f
Sammeln <40733234+Sammeln@users.noreply.github.com>
2026-08-13 10:59:54 +0300

    Add order-aware offcut evaluator tests

---
+++ Tests/OrderOffcutEvaluatorTests.cs
@@ -0,0 +1,76 @@
+﻿using AGR_PanelOptimizer.Core.Models;
+using AGR_PanelOptimizer.Core.Services;
+
+namespace AGR_PanelOptimizer.Core.Tests;
+
+public class OrderOffcutEvaluatorTests
+{
+    [Fact]
+    public void PanelOffcut_880mm_Is_Usable_When_OrderContains880mmValve()
+    {
+        var offcut = new PanelOffcut
+        {
+            Length = 1200,
+            Height = 880,
+            SourcePanelPosition = 5120
+        };
+
+        var orders = new[]
+        {
+            new ValveOrder { Height = 1280, Width = 2000, Quantity = 1 },
+            new ValveOrder { Height = 880, Width = 2000, Quantity = 1 }
+        };
+
+        var evaluator = new OffcutEvaluator();
+
+        var result = evaluator.IsUsable(offcut, orders);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void PanelOffcut_880mm_Is_Waste_When_OrderHasNo880mmValve()
+    {
+        var offcut = new PanelOffcut
+        {
+            Length = 1200,
+            Height = 880,
+            SourcePanelPosition = 5120
+        };
+
+        var orders = new[]
+        {
+            new ValveOrder { Height = 1200, Width = 2000, Quantity = 1 },
+            new ValveOrder { Height = 1220, Width = 2000, Quantity = 1 },
+            new ValveOrder { Height = 1280, Width = 2000, Quantity = 1 }
+        };
+
+        var evaluator = new OffcutEvaluator();
+
+        var result = evaluator.IsUsable(offcut, orders);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void PanelOffcut_Is_Not_Usable_When_Only_Different_Height_Is_Ordered()
+    {
+        var offcut = new PanelOffcut
+        {
+            Length = 1200,
+            Height = 880,
+            SourcePanelPosition = 5120
+        };
+
+        var orders = new[]
+        {
+            new ValveOrder { Height = 900, Width = 2000, Quantity = 1 }
+        };
+
+        var evaluator = new OffcutEvaluator();
+
+        var result = evaluator.IsUsable(offcut, orders);
+
+        Assert.False(result);
+    }
+}
