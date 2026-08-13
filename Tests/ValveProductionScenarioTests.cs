225610a7d3f5f3c313f7009d38df0171622d70e6
Sammeln <40733234+Sammeln@users.noreply.github.com>
2026-08-13 11:29:28 +0300

    Add panel-to-valve production integration tests

--- Tests/ValveProductionScenarioTests.cs
+++ Tests/ValveProductionScenarioTests.cs
@@ -1,4 +1,4 @@
-﻿using AGR_PanelOptimizer.Core.Enums;
+using AGR_PanelOptimizer.Core.Enums;
 using AGR_PanelOptimizer.Core.Models;
 using AGR_PanelOptimizer.Core.Services;
 
@@ -7,77 +7,121 @@ namespace AGR_PanelOptimizer.Core.Tests;
 public class ValveProductionScenarioTests
 {
     [Fact]
-    public void Produce_1280x2380_Valve_From_6000mm_Panel()
+    public void Produce_1280x1200_And_880x1200_Valves_Using_PanelOffcut()
     {
         // Arrange
-
         var panel = new Panel
         {
             Length = 6000,
             Height = 1200
         };
 
-        var blankCutter = new PanelCutter();
+        var panelCutter = new PanelCutter();
+        var cutResult = panelCutter.Cut(panel, 1280);
 
-        var blankResult = blankCutter.Cut(
-            panel,
-            1280);
+        var orders = new[]
+        {
+            new ValveOrder
+            {
+                Height = 1280,
+                Width = 1200,
+                Quantity = 1
+            },
+            new ValveOrder
+            {
+                Height = 880,
+                Width = 1200,
+                Quantity = 1
+            }
+        };
+
+        var materialPlanner = new OrderMaterialPlanner();
+        var materialPlan = materialPlanner.Prepare(cutResult, orders);
+
+        var materialPool = new MaterialPool();
+
+        foreach (var blank in materialPlan.Blanks)
+            materialPool.Add(blank);
+
+        var valvePlanner = new ValvePlanner(
+            new BlankPieceCutter(),
+            new ValveAssembler());
 
         // Act
+        var firstValve = valvePlanner.CreateValve(
+            orders[0],
+            materialPool,
+            minimumOffcut: 300);
 
-        var blankPieceCutter = new BlankPieceCutter();
-
-        var (piece1, remaining1) =
-            blankPieceCutter.Cut(
-                blankResult.Blanks[0],
-                1200);
-
-        var (piece2, remaining2) =
-            blankPieceCutter.Cut(
-                blankResult.Blanks[1],
-                1180);
+        var secondValve = valvePlanner.CreateValve(
+            orders[1],
+            materialPool,
+            minimumOffcut: 300);
 
         // Assert
+        Assert.Equal(1280, firstValve.Valve.Height);
+        Assert.Equal(1200, firstValve.Valve.Width);
+        Assert.Single(firstValve.Valve.Pieces);
 
-        Assert.Equal(4, blankResult.Blanks.Count);
+        Assert.Equal(880, secondValve.Valve.Height);
+        Assert.Equal(1200, secondValve.Valve.Width);
+        Assert.Single(secondValve.Valve.Pieces);
 
-        Assert.Equal(1200, piece1.Length);
-        Assert.Equal(1280, piece1.Height);
+        Assert.Empty(materialPlan.Waste);
+        Assert.Equal(3, materialPool.Blanks.Count);
+        Assert.All(
+            materialPool.Blanks,
+            blank => Assert.Equal(1280, blank.Height));
+    }
 
-        Assert.Equal(1180, piece2.Length);
-        Assert.Equal(1280, piece2.Height);
+    [Fact]
+    public void Prepare_OffcutAsWaste_WhenNoValveInOrderHasMatchingHeight()
+    {
+        // Arrange
+        var panel = new Panel
+        {
+            Length = 6000,
+            Height = 1200
+        };
 
-        Assert.Null(remaining1);
+        var panelCutter = new PanelCutter();
+        var cutResult = panelCutter.Cut(panel, 1280);
 
-        Assert.NotNull(remaining2);
-        Assert.Equal(20, remaining2!.Length);
-
-        Assert.Equal(EdgeType.Tongue, piece1.LeftEdge);
-        Assert.Equal(EdgeType.Groove, piece1.RightEdge);
-
-        Assert.Equal(EdgeType.Tongue, piece2.LeftEdge);
-        Assert.Equal(EdgeType.Cut, piece2.RightEdge);
-
-        Assert.Equal(EdgeType.Cut, remaining2.LeftEdge);
-        Assert.Equal(EdgeType.Groove, remaining2.RightEdge);
-
-        var assembler = new ValveAssembler();
-
-        var result = assembler.Assemble(
-            height: 1280,
-            width: 2380,
-            new[]
+        var orders = new[]
+        {
+            new ValveOrder
             {
-                piece1,
-                piece2
-            });
+                Height = 1200,
+                Width = 1200,
+                Quantity = 1
+            },
+            new ValveOrder
+            {
+                Height = 1220,
+                Width = 1200,
+                Quantity = 1
+            },
+            new ValveOrder
+            {
+                Height = 1280,
+                Width = 1200,
+                Quantity = 1
+            }
+        };
 
-        Assert.Equal(1280, result.Valve.Height);
-        Assert.Equal(2380, result.Valve.Width);
-        Assert.Equal(2, result.Valve.Pieces.Count);
-        var offcutEvaluator = new OffcutEvaluator();
+        var materialPlanner = new OrderMaterialPlanner();
 
-        Assert.False(
-            !offcutEvaluator.IsUsable(remaining2!, remaining2.Length));
+        // Act
+        var materialPlan = materialPlanner.Prepare(cutResult, orders);
+
+        // Assert
+        Assert.Equal(4, materialPlan.Blanks.Count);
+        Assert.Single(materialPlan.Waste);
+
+        var waste = materialPlan.Waste[0];
+
+        Assert.Equal(1200, waste.Length);
+        Assert.Equal(880, waste.Height);
+        Assert.Equal(4, waste.SourcePanelPosition);
     }
 }
